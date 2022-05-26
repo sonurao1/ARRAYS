2ceb5ceebf8c6ad0bd2ea9719c112f78696575ec
shreealagh <104250062+shreealagh@users.noreply.github.com>
2022-05-26 21:43:55 +0530

    Create bubblesort.cpp

---
+++ bubblesort.cpp
@@ -0,0 +1,30 @@
+//bubble sort
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    //loop for iterations
+    for(int i=0;i<=n-2;i++)
+    {
+        //loop for comparisons
+        for(int j=0;j<=n-2-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";    
+    }
+
+    return 0;
+}
